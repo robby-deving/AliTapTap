45bca26339b859dd990b70d13bbc270604f6ed62
Vicente Bercasio <vicentetobio00@gmail.com>
2025-02-21 23:15:17 +0800

    fix

--- client/app/success.tsx
+++ client/app/success.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
+
 const SuccessScreen: React.FC = () => {
     const router = useRouter();
+    const navigation = useNavigation();
+    const { orderNumber, paymentTime, quantity, totalAmount } = useLocalSearchParams();
 
     const handleGoBack = () => {
         router.push('/');
@@ -11,8 +14,20 @@ const SuccessScreen: React.FC = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.successText}>Success!</Text>
+            <Image 
+                source={require('../assets/images/success.png')} 
+                resizeMode="contain" // Correctly using resizeMode as a prop
+                style={styles.successImage} // Use the styles defined in StyleSheet
+            />
+
+            <Text style={styles.successText}>Order Success!</Text>
             <Text style={styles.messageText}>Your transaction was completed successfully.</Text>
+            
+            <Text style={styles.detailText}>Order Number: {orderNumber}</Text>
+            <Text style={styles.detailText}>Payment Time: {paymentTime}</Text>
+            <Text style={styles.detailText}>Quantity: {quantity}</Text>
+            <Text style={styles.detailText}>Total Amount: {totalAmount}</Text>
+
             <Button title="Go to Home" onPress={handleGoBack} />
         </View>
     );
@@ -25,6 +40,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#fff',
     },
+    successImage: {
+        width: 100, // Adjust the width of the image
+        height: 100, // Adjust the height of the image
+        marginBottom: 20,
+    },
     successText: {
         fontSize: 32,
         fontWeight: 'bold',
@@ -35,6 +55,10 @@ const styles = StyleSheet.create({
         fontSize: 18,
         textAlign: 'center',
         marginBottom: 20,
+    },
+    detailText: {
+        fontSize: 16,
+        marginBottom: 10,
     },
 });
 
