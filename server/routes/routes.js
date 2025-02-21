58765946bf31667f4f1acd674e7f88111a623b69
Vicente Bercasio <vicentetobio00@gmail.com>
2025-02-21 15:05:54 +0800

    Fix routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('./user.route');
-<<<<<<< HEAD
 const authRoute = require('./auth.route');
 const cardDesignRoutes = require('./carddesign.route');
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require('./product.route');  
 const transactionRoutes = require('./transaction.route');  
 const orderRoutes = require('./order.route');
+
+const paymentRoutes = require('./payment.route');
 
 const base = "/api/v1";
 
@@ -18,15 +19,7 @@ router.use(`${base}/dashboard`, dashboardRoutes);
 router.use(`${base}/products`, productRoutes);  
 router.use(`${base}/transactions`, transactionRoutes);  
 router.use(`${base}/orders`, orderRoutes);
-=======
-const paymentRoutes = require('./payment.route');
 
-const base = "/api/v1"
-
-router.use(`${base}/users`, userRoutes);
-router.use(`${base}/pay`, paymentRoutes);
-
-//http://localhost:5000/api/v1/pay/payment
->>>>>>> f/payment
+router.use(`${base}/pay`, paymentRoutes); 
 
 module.exports = router;
