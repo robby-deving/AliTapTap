8752f7f639c690ec71b5401eb1aa1dbba1518a6b
Robby <jrcr2022-2647-26244@bicol-u.edu.ph>
2025-02-27 22:12:18 +0800

    sidebar done starting designing dashboard

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/sidebar';
+import Chats from './pages/Chats';
+import Dashboard from './pages/Dashboard';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
 
 function App() {
-
-  return (
-    <>
-      <div className="h-full">
-        
-      </div>
-    </>
-  )
+    return (
+        <Router>
+            <div className="flex flex-row h-screen">
+                <Sidebar />
+                <div className="flex-grow">
+                    <Routes>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/orders" element={<Orders />} />
+                        <Route path="/products" element={<Products />} />
+                        <Route path="/chats" element={<Chats />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
