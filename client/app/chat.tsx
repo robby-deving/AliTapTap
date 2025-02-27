2795db61a09e509eff911ead45517c918a1e7087
hereskira <shakiraregalado@gmail.com>
2025-02-27 18:12:35 +0800

    Add frontend design

--- client/app/chat.tsx
+++ client/app/chat.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { View, TextInput, Button, Text, ScrollView } from "react-native";
+import { View, TextInput, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import io, { Socket } from "socket.io-client";
+import { Header } from "../components/Header";
 
 export default function ChatScreen() {
   const [chatMessage, setChatMessage] = useState("");
@@ -8,7 +9,7 @@ export default function ChatScreen() {
   const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    const newSocket = io("http://192.168.1.16:4000");
+    const newSocket = io("http://192.168.1.8:4000");
     setSocket(newSocket);
 
     newSocket.on("connect", () => {
@@ -16,7 +17,7 @@ export default function ChatScreen() {
     });
 
     newSocket.on("message", (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
+      setMessages((prevMessages) => [...prevMessages, msg]); // Append messages normally
     });
 
     return () => {
@@ -32,23 +33,38 @@ export default function ChatScreen() {
   };
 
   return (
-    <View className="flex-1 justify-center items-center bg-white p-5">
-      <ScrollView className="w-full p-2">
-        {messages.map((msg, index) => (
-          <Text key={index} className="bg-gray-200 p-2 rounded mb-2">
-            {msg}
-          </Text>
-        ))}
-      </ScrollView>
-      <TextInput
-        autoCorrect={false}
-        value={chatMessage}
-        onChangeText={setChatMessage}
-        placeholder="Type a message..."
-        className="border border-gray-300 p-2 rounded w-full"
-        onSubmitEditing={submitChatMessage}
-      />
-      <Button title="Send" onPress={submitChatMessage} />
+    <View className="flex-1 relative bg-white">
+      <Header />
+
+      <View className="flex-1 bg-white p-5">
+        {/* Messages List (Appears Just Above Input Box) */}
+        <View className="flex-1 w-full">
+          <FlatList
+            data={messages}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <Text className="bg-gray-100 py-3 rounded mb-2 min-h-12 text-xl px-4">{item}</Text>
+            )}
+            contentContainerStyle={{ flexGrow: 1, justifyContent: "flex-end" }} // Moves messages to the bottom
+            keyboardShouldPersistTaps="handled"
+          />
+        </View>
+
+        {/* Message Input & Send Button */}
+        <View className="flex-row items-center w-full p-2">
+          <TextInput
+            autoCorrect={false}
+            value={chatMessage}
+            onChangeText={setChatMessage}
+            placeholder="Type a message..."
+            className="flex-1 border border-gray-300 p-2 rounded h-12 text-gray-700"
+            onSubmitEditing={submitChatMessage}
+          />
+          <TouchableOpacity onPress={submitChatMessage} className="ml-2">
+            <Image source={require("../assets/images/Vector.png")} style={{ width: 24, height: 20 }} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
