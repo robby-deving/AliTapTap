30eb4d382ef882759800d759685130f0f78dc866
V7igcv <igcv2022-3306-76073@bicol-u.edu.ph>
2025-03-07 16:07:35 +0800

    added orders label

--- admin/src/pages/Orders.tsx
+++ admin/src/pages/Orders.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export default function Orders() {
     return (
-        <div>
-            <h1>Orders</h1>
+        <div className='overflow-auto w-full flex p-10 gap-10'>
+            <div className='flex-1 '>
+                {/* Top Section */}
+                <div className="flex justify-between items-center">
+                    <h1 className='text-[2.25rem] font-bold pb-5'>Orders</h1>
+                    <div>
+                    {/* <label className="mr-2 text-sm font-medium">Filter by Date:</label>
+                    <input type="date" className="border p-2 rounded-lg" /> */}
+                    </div>
+                </div>
+
+                {/* Bottom Section */}
+                
+            </div>
         </div>
+        
+        
     );
 }
