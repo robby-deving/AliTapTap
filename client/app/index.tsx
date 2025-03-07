9b5e347cbf635d33a02d569453076f24cce7644e
Shakira Regalado <136966945+hereskira@users.noreply.github.com>
2025-03-07 22:03:03 +0800

    new branch

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [checkingAuth, setCheckingAuth] = useState(true);
 
-  // ✅ Load authentication token on app start
+  // ✅ Load authentication token on app starts
   useEffect(() => {
     const checkAuth = async () => {
       const token = await AsyncStorage.getItem("token");
